e8822113ddc5c638710cc5cefaa39d6cbf178a54
XKC99 <105143361+XKC99@users.noreply.github.com>
2022-06-14 19:40:47 +0800

    第二次

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-    public string "First"
+    public string "Second"
     // Start is called before the first frame update
     void Start()
     {
